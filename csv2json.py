7f60e36db5eea98b347c3d5c177c700f772e03ed
Hoel Kervadec <hoel@kervadec.science>
2023-05-23 16:25:44 +0200

    Add support for short papers list

--- csv2json.py
+++ csv2json.py
@@ -13,22 +13,23 @@ ORAL_SESSIONS: Final[list[str]] = ["(midl board session)", "Computer-assisted di
                                    "Unsupervised/Weakly supervised methods"]
 
 if __name__ == "__main__":
+        papers: list[Paper] = []
+
         midl_orals_df = pd.read_csv("midl_oral_sessions.csv")
 
         session_map: dict[str, str] = {}
         for index, row in midl_orals_df.iterrows():
                 session_map[row["title"]] = row["session"]
 
-        pprint(session_map)
-
+        # pprint(session_map)
         assert set(session_map.values()) == set(ORAL_SESSIONS)
+        print(f">>> Loaded {len(set(session_map.values()))} sessions")
 
         full_papers_df = pd.read_csv("full_papers.csv")
 
-        papers: list[Paper] = []
-
+        title: str
         for _, full_row in full_papers_df.iterrows():
-                title: str = full_row["title"]
+                title = full_row["title"]
                 is_oral = title in session_map
 
                 schedule: str = session_map[title] if is_oral else ""
@@ -38,7 +39,21 @@ if __name__ == "__main__":
                               short="False", abstract=full_row["abstract"], ignore_schedule=True)
                 papers.append(paper)
 
+        print(f">>> Loaded {len(full_papers_df)} full papers")
+
+        short_papers_df = pd.read_csv("short_papers.csv")
+        for _, full_row in full_papers_df.iterrows():
+                title = full_row["title"]
+
+                paper = Paper(id=full_row["number"], title=title, authors=full_row["authors"],
+                              or_id="", oral="False",
+                              short="True", abstract="", ignore_schedule=True)
+                papers.append(paper)
+
+        print(f">>> Loaded {len(short_papers_df)} short papers")
+
+        print(f">>> Writing {len(papers)} to papers.json...")
         with open("papers.json", 'w') as sink:
-                json.dump({p.id: p for p in papers}, sink, cls=PaperEncoder, indent=4, sort_keys=True)
+                json.dump({p.conf_id: p for p in papers}, sink, cls=PaperEncoder, indent=4, sort_keys=True)
 
         # print(full_papers_csv)
