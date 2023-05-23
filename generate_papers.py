c889855be0aaa7329877ff1a22bd8ca2e0c085ad
Hoel Kervadec <hoel@kervadec.science>
2023-05-23 16:18:52 +0200

    First script generating paper pages

---
+++ generate_papers.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3.11
+
+import json
+from sys import argv
+from pathlib import Path
+from operator import attrgetter
+
+from paper import Paper
+
+
+if __name__ == "__main__":
+    assert len(argv) == 5
+
+    template_path: Path = Path(argv[1])
+    assert template_path.exists()
+
+    papers_path: Path = Path(argv[2])
+    assert papers_path.exists()
+
+    dest_path: Path = Path(argv[3])
+
+    root_slides: Path = Path(argv[4])
+
+    raw_papers: dict[str, dict]
+    with open(papers_path, 'r') as pf:
+        raw_papers = json.load(pf)
+
+    papers: dict[int, Paper] = {int(k): Paper(**v, ignore_schedule=True) for (k, v) in raw_papers.items()}
+
+    with open(template_path, 'r') as f:
+        empty_template: str = f.read()
+
+    paper: Paper
+    for paper in papers.values():
+        result: str = empty_template[:]
+
+        result = result.replace("CONF_ID", paper.conf_id)
+        result = result.replace("SMALLID", paper.conf_id.casefold())
+        result = result.replace("TITLE", paper.title)
+        result = result.replace("AUTHORS", ", ".join(paper.authors))
+        result = result.replace("ORID", paper.or_id)
+        result = result.replace("ABSTRACT", paper.sanitized_abstract)
+        result = result.replace("SCHEDULE", "<br>".join(paper.schedule))
+
+        if paper.award:
+            result = result.replace("AWARD", f"## {paper.award}")
+        else:
+            result = result.replace("AWARD", "")
+
+        # slides_path: Path = Path(paper.slides)
+
+        oral_text: str
+        if paper.oral:
+            oral_text = "Oral presentation"
+        elif paper.poster:
+            oral_text = "Spotlight presentation"
+        elif paper.short:
+            oral_text = "Short paper"
+
+        result = result.replace("ORAL", oral_text)
+
+        with open((dest_path / Path(paper.url)).with_suffix(".md"), 'w') as sink:
+            sink.write(result)
